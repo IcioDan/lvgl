820e8ab71b9b4d343206c88353f44d7ae1421996
IcioDan <168101256+IcioDan@users.noreply.github.com>
2025-09-06 15:41:29 +0200

    Update lv_conf.h

--- src/lv_conf.h
+++ src/lv_conf.h
@@ -36,23 +36,4 @@
 #define LV_TXT_ENC LV_TXT_ENC_UTF8
 
 #endif /* fine lv_conf.h */
-/*
-#ifndef LV_CONF_H
-#define LV_CONF_H
 
-#define LV_COLOR_DEPTH 16
-#define LV_COLOR_16_SWAP 0
-#define LV_MEM_SIZE (48U * 1024U)
-#define LV_USE_PERF_MONITOR 0
-#define LV_USE_MEM_MONITOR 0
-#define LV_USE_LOG 0
-#define LV_FONT_MONTSERRAT_14 1
-#define LV_THEME_DEFAULT_DARK 0
-#define LV_USE_ASSERT_NULL 0
-#define LV_USE_ASSERT_MALLOC 0
-#define LV_USE_ASSERT_STYLE 0
-#define LV_USE_ASSERT_MEM_INTEGRITY 0
-#define LV_USE_ASSERT_OBJ 0
-
-#endif
-*/
